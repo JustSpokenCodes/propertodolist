37d99a03034bbdea826479096346f04a76ea4732
Joshua Stevens <jastevns93@gmail.com>
2020-08-18 21:04:42 -0700

    finished index.js

--- properlist/src/index.js
+++ properlist/src/index.js
@@ -1,17 +1,107 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './src/serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./styles.css";
+import ShoppingList from "./components/ShoppingList";
+import ShoppingListForm from "./components/ShoppingListForm";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const anime = [
+  {
+    name: "Megalobox",
+    id: 1,
+    watched: false
+  },
+  {
+    name: "Highschool DxD",
+    id: 2,
+    watched: true
+  },
+  {
+    name: "The God of High School",
+    id: 3,
+    watched: false
+  }
+];
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      anime
+    };
+  }
+
+  addAnime = item => {
+    let newAnime = {
+      name: item,
+      id: Date.now(),
+      watched: false
+    };
+
+    const sorted = [...this.state.anime, newAnime].sort(function(a, b) {
+      let nameA = a.name.toUpperCase();
+      let nameB = b.name.toUpperCase();
+
+      return nameA > nameB ? 1 : -1;
+    });
+
+    this.setState({ anime: sorted });
+  };
+
+  toggleAnime = id => {
+    let anime = this.state.anime.slice();
+    anime = anime.map(anime => {
+      if (anime.id === id) {
+        item.watched = !item.watched;
+        return item;
+      } else {
+        return item;
+      }
+    });
+    this.setState({anime});
+  };
+
+  clearComplete = id => {
+    const filtered = this.state.anime.filter(item => {
+      return item.watched === false;
+    });
+    this.setState({anime: filtered});
+  };
+
+  render(){
+
+    if(this.state.anime.length < 1) {
+      return (
+        <div className = "App">
+          <div className="header">
+            <h1>Shopping List </h1>
+            <ShoppingListForm addAnime={this.addAnime} />
+          </div>
+          <h2>What Else Have You Seen?!</h2>
+        </div>
+      )
+    }
+
+    return (
+      <div className="App">
+        <div className="header">
+          <h1>Shopping List</h1>
+          <ShoppingListForm addAnime={this.addAnime} />
+        </div>
+        <ShoppingList
+          anime={this.state.anime}
+          toggleAnime={this.toggleAnime}
+        />
+        <div>
+          <button onClick={thi.clearComplete}
+          className="clear=btn">
+            Clear Watched
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
